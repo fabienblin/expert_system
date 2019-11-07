6cb7a1e8aee6cc2dc2f0fc7a195871b834c256a2
Fabien Blin <fablin@z3r4p1.le-101.fr>
2019-11-07 18:08:45 +0100

    precedence fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,7 @@
 package main
 
 func main() {
-
 	parse()
-
-	// default fact Z={"Z", false, false}
-	// two methods to access any fact.
-	// 1: from env.factList (parser)
-	// 2: from env.trees (engine)
-	env.factList["Z"].isKnown = true
-	env.trees[0].right.fact.isTrue = true
-
-	// trees
-
-	// tree
-
+	printNode(env.tree, 4)
 	engine()
 }
